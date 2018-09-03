463fe1eea3d23147220ac84839251d6d1426344e
sousuke-tanaka <sousuke-tanaka@outlook.com>
2018-09-03 16:21:26 -0600

    Adds photos, minor style tweaks

--- src/util/photoData.js
+++ src/util/photoData.js
@@ -4,41 +4,98 @@ export const photoData = {
     description: "moraine lake",
     filename: "DSC_4055.JPG",
     orientation: "portrait",
-    tags: ["banff"]
+    tags: ["banff", "hiking", "canada"]
   },
   "2": {
     id: 2,
     description: "mt elbert",
     filename: "IMG_0563.JPG",
     orientation: "landscape",
-    tags: ["stars", "colorado"]
+    tags: ["stars", "colorado", "14er", "hiking"]
   },
   "3": {
     id: 3,
-    description: "square top lakes",
+    description: "ollie from square top lakes",
     filename: "IMG_1897.jpg",
     orientation: "landscape",
-    tags: ["colorado"]
-  },
-  "4": {
-    id: 4,
-    description: "square top lakes",
-    filename: "IMG_1897.jpg",
-    orientation: "landscape",
-    tags: ["colorado"]
+    tags: ["colorado", "hiking", "ollie"]
   },
   "5": {
     id: 5,
-    description: "moraine lake",
-    filename: "DSC_4055.JPG",
-    orientation: "portrait",
-    tags: ["banff"]
+    description: "milky way in chaffee county",
+    filename: "IMG_1954.jpg",
+    orientation: "landscape",
+    tags: ["colorado", "camping", "milky way", "stars"]
   },
   "6": {
     id: 6,
-    description: "mt elbert",
-    filename: "IMG_0563.JPG",
+    description: "milky way in chaffee county",
+    filename: "IMG_1967.jpg",
+    // orientation: "portrait",    ,
     orientation: "landscape",
-    tags: ["stars", "colorado"]
+    tags: ["colorado", "camping", "milky way", "stars"]
+  },
+  "7": {
+    id: 7,
+    description: "campsite at chaffee county",
+    filename: "IMG_1977.jpg",
+    orientation: "landscape",
+    tags: ["colorado", "camping", "stars"]
+  },
+  "8": {
+    id: 8,
+    description: "lake ann hike",
+    filename: "IMG_2008.jpg",
+    orientation: "landscape",
+    tags: ["colorado", "hiking"]
+  },
+  "9": {
+    id: 9,
+    description: "ollie on lake ann hike",
+    filename: "IMG_2045.jpg",
+    orientation: "landscape",
+    tags: ["colorado", "hiking", "ollie"]
+  },
+  "9": {
+    id: 9,
+    description: "ollie on lake ann hike",
+    filename: "IMG_2048.jpg",
+    orientation: "landscape",
+    tags: ["colorado", "hiking", "ollie", "lake"]
+  },
+  "10": {
+    id: 10,
+    description: "ollie on grays peak",
+    filename: "IMG_0743.jpg",
+    orientation: "landscape",
+    tags: ["colorado", "hiking", "ollie", "14er"]
+  },
+  "11": {
+    id: 11,
+    description: "sulphur mountain",
+    filename: "DSC_4042.jpg",
+    orientation: "landscape",
+    tags: ["banff", "hiking", "canada"]
+  },
+  "12": {
+    id: 12,
+    description: "sulphur mountain",
+    filename: "DSC_4044.jpg",
+    orientation: "landscape",
+    tags: ["banff", "hiking", "canada"]
+  },
+  "13": {
+    id: 13,
+    description: "moraine lake",
+    filename: "DSC_4057.jpg",
+    orientation: "landscape",
+    tags: ["banff", "hiking", "canada", "lake"]
+  },
+  "14": {
+    id: 14,
+    description: "peyto lake",
+    filename: "DSC_4068.jpg",
+    orientation: "landscape",
+    tags: ["banff", "hiking", "canada", "lake"]
   }
 };
