b817e6fe39d748ee8cd1fb4e9c4c380227d84018
sousuke-tanaka <sousuke-tanaka@outlook.com>
2018-09-03 09:02:50 -0600

    Adds mobile tag menu

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,27 +1,67 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
+import arrowIconImg from "../assets/arrow-icon.svg";
 
 class Tags extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showTagMenu: false
+    };
+  }
+
+  toggleTagMenu = () => {
+    this.setState({ showTagMenu: !this.state.showTagMenu });
+  };
+
   render() {
     const { handleTagClick, tags } = this.props;
+    const { showTagMenu } = this.state;
 
     return (
-      <div className="Tags-container">
-        <span className="Tags-title">tags:</span>
-        {Object.keys(tags).map(tag => {
-          console.log(tag);
-          return (
-            <button
-              className={cx("Tags-tag", { "Tags-active": tags[tag] })}
-              key={tag}
-              onClick={() => handleTagClick(tag)}
-            >
-              {tag}
-            </button>
-          );
-        })}
-      </div>
+      <React.Fragment>
+        {showTagMenu ? (
+          <div className="Tags-container">
+            <div>
+              <img
+                alt="Tag menu toggle"
+                className="Tags-menuToggle"
+                onClick={this.toggleTagMenu}
+                src={arrowIconImg}
+              />
+            </div>
+            <span className="Tags-title">filter by tags:</span>
+            <div className="Tags-group">
+              {Object.keys(tags).map(tag => {
+                console.log(tag);
+                return (
+                  <button
+                    className={cx("Tags-tag", { "Tags-active": tags[tag] })}
+                    key={tag}
+                    onClick={() => handleTagClick(tag)}
+                  >
+                    {tag}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        ) : (
+          <div className="Tags-collapsedTagMenu">
+            <img
+              alt="Tag menu toggle"
+              className={cx("Tags-menuToggle", {
+                "Tags-menuToggleCollapsed": !this.state.showTagMenu
+              })}
+              onClick={this.toggleTagMenu}
+              src={arrowIconImg}
+            />
+            <span className="Tags-title">tag menu</span>
+          </div>
+        )}
+      </React.Fragment>
     );
   }
 }
